64aa7042d7a234b10964b0e54ecec89bf4b8d7d6
Maksim Anisimov <46654281+m9117882424@users.noreply.github.com>
2026-05-20 16:48:16 +0300

    Use parallel RU decont runner by default

--- compare_ru_decont.bat
+++ compare_ru_decont.bat
@@ -3,6 +3,6 @@ chcp 65001 > nul
 
 cd /d "%~dp0"
 
-py ru_decont.py --excel "excel_input\input.xlsx" --pdf-dir "pp_ru"
+py ru_decont_fast.py --excel "excel_input\input.xlsx" --pdf-dir "pp_ru" --workers 3
 
 pause
